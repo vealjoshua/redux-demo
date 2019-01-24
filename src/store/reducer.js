004398a6a232e9b73433642bae8d3cb36a5e65da
Joshua Veal <jkv0702@daugherty.com>
2019-01-24 10:28:23 -0600

    how-to-use.txt

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,26 +3,12 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        counter: state.counter + 1
-      };
-    case "DECREMENT":
-      return {
-        counter: state.counter - 1
-      };
-    case "ADD":
-      return {
-        counter: state.counter + action.value
-      };
-    case "SUBTRACT":
-      return {
-        counter: state.counter - action.value
-      };
-    default:
-      return state;
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1
+    };
   }
+  return state;
 };
 
 export default reducer;
