3747d048c255aba18d26dc2cbc82a529735fc0d3
Joshua Veal <jkv0702@daugherty.com>
2019-01-24 11:30:43 -0600

    added switch cases.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,33 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === "INCREMENT") {
-    return {
-      counter: state.counter + 1
-    };
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    case "ADD":
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+    case "SUBTRACT":
+      return {
+        ...state,
+        counter: state.counter - action.value
+      };
+    default:
+      return state;
   }
-  return state;
 };
 
 export default reducer;
